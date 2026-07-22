e89325cd1afead8e27b4013262c1d23d7b518150
Chè <22010087@st.phenikaa-uni.edu.vn>
2026-07-22 14:07:31 +0700

    bt1 ss12

---
+++ product-service/src/test/java/com/example/demorediscache/DemoRedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demorediscache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRedisCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
