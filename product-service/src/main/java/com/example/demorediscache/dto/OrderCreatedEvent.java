09d6859b505df746026fd1f5490f713db56fed8d
Chè <119411856+1tra2016@users.noreply.github.com>
2026-07-22 15:33:48 +0700

    Delete product-service directory

--- product-service/src/main/java/com/example/demorediscache/dto/OrderCreatedEvent.java
+++
@@ -1,4 +0,0 @@
-package com.example.demorediscache.dto;
-
-public class OrderCreatedEvent {
-}
--- product-service/src/main/java/com/example/demorediscache/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demorediscache.repository;
-
-import com.example.demorediscache.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
